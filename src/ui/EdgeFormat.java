ebb5ae203d5d26ec3583ce4bdfad9eab150c6729
menzs2@bfh.ch <menzs2@bfh.ch>
2013-10-19 13:21:47 +0200

    added EdgeFormat and VertexFormatClass

---
+++ src/ui/EdgeFormat.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ui;
+
+/**
+ *
+ * @author Stephan_2
+ */
+public class EdgeFormat {
+
+    private boolean isDirected;
+    private boolean active;
+    private boolean included;
+    private String label;
+}

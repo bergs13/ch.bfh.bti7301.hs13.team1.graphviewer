53a16191250d791b9e8f117cd6289e866bac50f7
menzs2@bfh.ch <menzs2@bfh.ch>
2013-10-19 14:46:43 +0200

    worked on EdgeFormat and VertexFormatClass

--- src/defs/EdgeFormat.java
+++ src/defs/EdgeFormat.java
@@ -1,8 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package defs;
 
-public class EdgeFormat 
-{
-	public EdgeFormat() 
-	{
-	}
+/**
+ *
+ * @author Stephan_2
+ */
+public class EdgeFormat {
+
+    private boolean isDirected;
+    private boolean active;
+    private boolean included;
+    private String label;
 }
--- src/defs/VertexFormat.java
+++ src/defs/VertexFormat.java
@@ -1,8 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package defs;
 
-public class VertexFormat 
-{
-	public VertexFormat() 
-	{
-	}
+import java.awt.Point;
+import java.awt.Color;
+
+/**
+ *
+ * @author Stephan_2
+ */
+public class VertexFormat {
+    
+    //variabels
+    private Color activeColor;
+    private Color visitedColor;
+    private Color unvisitedColor;
+    private Point centerPoint = new Point(100, 100);
+    private String label;
+    private boolean active = false;
+    private boolean visited = false;
+    
+    //default values
+    private static final Color DEFAULT_ACTIVE_COLOR = new Color(255, 0 ,0);
+    private static final String DEFAULT_LABEL = "DV";
+    
+    public VertexFormat() {
+        this.setDefault();
+    }
+
+    
+    
+    public void setDefault(){
+        label= DEFAULT_LABEL;
+        active = false;
+        activeColor = DEFAULT_ACTIVE_COLOR;
+        
+        
+    }
 }

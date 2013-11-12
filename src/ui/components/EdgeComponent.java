a2be7ee327cb7fa3d9db617fd7e57bbb3596c1d8
bergs13 <bergs13@bfh.ch>
2013-11-12 21:58:55 +0100

    intermediate state gui (vertices/edges)

--- src/ui/components/EdgeComponent.java
+++ src/ui/components/EdgeComponent.java
@@ -10,12 +10,10 @@ import java.awt.Rectangle;
 import java.awt.RenderingHints;
 import java.awt.Shape;
 import java.awt.geom.Line2D;
-
 import javax.swing.BorderFactory;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
-import javax.swing.JTextField;
-
+import defs.EdgeFormat;
 import logic.VisualizationCalculator;
 import logic.extlib.Edge;
 
@@ -31,14 +29,14 @@ public class EdgeComponent<E> extends JComponent {
 
 	// Constant values
 	private static final int ARROWTRIANGLEWIDTH = 10;
-	private static final int ARROWTRIANGLEHEIGHT = 5;
-	private static final int SHAPEBORDER = 1;
+	private static final int ARROWTRIANGLEHEIGHT = 15;
 
 	// End of constant values
 
 	// Constructors
 	public EdgeComponent(Edge<E> edge) {
-		this.edge = edge;
+		this.edge = edge;	
+		this.shape = new Rectangle(300,300);
 	}
 
 	// End of constructors
@@ -56,8 +54,20 @@ public class EdgeComponent<E> extends JComponent {
 
 		// Definierbare Sachen aus Edge-Format (Überschreiben wenn Format
 		// vorhanden)
-		Point fromPoint = new Point(0, 0);
-		Point toPoint = new Point(100, 100);
+		Object o = edge.get(EdgeFormat.FORMAT);
+		EdgeFormat edgeFormat = EdgeFormat.class.isInstance(o) ? EdgeFormat.class.cast(o) : null;
+		Point fromPoint;
+		Point toPoint;
+		if(null != edgeFormat)
+		{
+			fromPoint = edgeFormat.getFromPoint();
+			toPoint = edgeFormat.getToPoint();
+		}
+		else
+		{
+			 fromPoint = new Point(100,100);
+			 toPoint = new Point(0,0);
+		}
 		Color inactiveColor = new Color(0, 0, 255);
 		Color activeColor = new Color(255, 0, 0);
 		boolean active = true;
@@ -66,99 +76,123 @@ public class EdgeComponent<E> extends JComponent {
 
 		g2.setColor(active ? activeColor : inactiveColor);
 
-		// Polygon für Shape der Component
-		Polygon shapePolygon = new Polygon();
-
 		// Linie immer
 		Line2D line = new Line2D.Double(fromPoint.x, fromPoint.y, toPoint.x,
 				toPoint.y);
 		g2.draw(line);
 
 		// Optionale Shapes
-		// Pfeil als Dreieck wenn gerichtet
-		if (directed) {
-			// Wird benötigt für Fallunterscheidungen
-			int deltaX = fromPoint.x - toPoint.x;
-			int deltaY = fromPoint.y - toPoint.y;
+		if (directed || weighted) {
+			// Polygon für Shape der Component
+			Polygon shapePolygon = new Polygon();
+			shapePolygon.addPoint(fromPoint.x, fromPoint.y);
+			shapePolygon.addPoint(toPoint.x, toPoint.y);
+			shapePolygon.npoints = 2;
 
-			// Seitliche Ecken
-			Point sidePointOne = new Point();
-			Point sidePointTwo = new Point();
+			// Pfeil als Dreieck wenn gerichtet
+			if (directed) {
+				// Wird benötigt für Fallunterscheidungen
+				int deltaX = Math.abs(fromPoint.x - toPoint.x);
+				int deltaY = Math.abs(fromPoint.y - toPoint.y);
 
-			// Spezialfälle
-			// Senkrecht
-			if (deltaX == 0) {
-				// Richtung
-				if (toPoint.y > fromPoint.y) {
-					sidePointOne.setLocation(
-							toPoint.x - ARROWTRIANGLEWIDTH / 2, toPoint.y
-									- ARROWTRIANGLEHEIGHT);
-					sidePointTwo.setLocation(
-							toPoint.x + ARROWTRIANGLEWIDTH / 2, toPoint.y
-									- ARROWTRIANGLEHEIGHT);
-				} else {
-					sidePointOne.setLocation(
-							toPoint.x - ARROWTRIANGLEWIDTH / 2, toPoint.y
-									+ ARROWTRIANGLEHEIGHT);
-					sidePointTwo.setLocation(
-							toPoint.x + ARROWTRIANGLEWIDTH / 2, toPoint.y
-									+ ARROWTRIANGLEHEIGHT);
+				// Seitliche Ecken
+				Point sidePointOne = new Point();
+				Point sidePointTwo = new Point();
+
+				// Spezialfälle
+				// Senkrecht
+				if (deltaX == 0) {
+					// Richtung
+					if (toPoint.y > fromPoint.y) {
+						sidePointOne.setLocation(toPoint.x - ARROWTRIANGLEWIDTH
+								/ 2, toPoint.y - ARROWTRIANGLEHEIGHT);
+						sidePointTwo.setLocation(toPoint.x + ARROWTRIANGLEWIDTH
+								/ 2, toPoint.y - ARROWTRIANGLEHEIGHT);
+					} else {
+						sidePointOne.setLocation(toPoint.x - ARROWTRIANGLEWIDTH
+								/ 2, toPoint.y + ARROWTRIANGLEHEIGHT);
+						sidePointTwo.setLocation(toPoint.x + ARROWTRIANGLEWIDTH
+								/ 2, toPoint.y + ARROWTRIANGLEHEIGHT);
+					}
 				}
-			}
-			// Waagrecht
-			else if (deltaY == 0) {
-				// Richtung
-				if (toPoint.x > fromPoint.x) {
-					sidePointOne.setLocation(toPoint.x - ARROWTRIANGLEHEIGHT,
-							toPoint.y - ARROWTRIANGLEWIDTH / 2);
-					sidePointTwo.setLocation(toPoint.x - ARROWTRIANGLEHEIGHT,
-							toPoint.y + ARROWTRIANGLEWIDTH / 2);
-				} else {
-					sidePointOne.setLocation(toPoint.x + ARROWTRIANGLEHEIGHT,
-							toPoint.y - ARROWTRIANGLEWIDTH / 2);
-					sidePointTwo.setLocation(toPoint.x + ARROWTRIANGLEHEIGHT,
-							toPoint.y + ARROWTRIANGLEWIDTH / 2);
+				// Waagrecht
+				else if (deltaY == 0) {
+					// Richtung
+					if (toPoint.x > fromPoint.x) {
+						sidePointOne.setLocation(toPoint.x
+								- ARROWTRIANGLEHEIGHT, toPoint.y
+								- ARROWTRIANGLEWIDTH / 2);
+						sidePointTwo.setLocation(toPoint.x
+								- ARROWTRIANGLEHEIGHT, toPoint.y
+								+ ARROWTRIANGLEWIDTH / 2);
+					} else {
+						sidePointOne.setLocation(toPoint.x
+								+ ARROWTRIANGLEHEIGHT, toPoint.y
+								- ARROWTRIANGLEWIDTH / 2);
+						sidePointTwo.setLocation(toPoint.x
+								+ ARROWTRIANGLEHEIGHT, toPoint.y
+								+ ARROWTRIANGLEWIDTH / 2);
+					}
 				}
-			}
-			// Allgemeiner Fall
-			else {
-				Point[] leftAndRightPoints = VisualizationCalculator
-						.getPointsOnNormalVectorsOfStraightLine(toPoint,
-								fromPoint, ARROWTRIANGLEHEIGHT,
-								ARROWTRIANGLEWIDTH / 2);
-				sidePointOne = leftAndRightPoints[0];
-				sidePointTwo = leftAndRightPoints[1];
+				// Allgemeiner Fall
+				else {
+					Point[] leftAndRightPoints = VisualizationCalculator
+							.getPointsOnNormalVectorsOfStraightLine(toPoint,
+									fromPoint, ARROWTRIANGLEHEIGHT,
+									ARROWTRIANGLEWIDTH / 2);
+					sidePointOne = leftAndRightPoints[0];
+					sidePointTwo = leftAndRightPoints[1];
+				}
+
+				// Daten für Pfeil bekannt, Pfeil zeichnen
+				int[] arrowX = new int[] { toPoint.x, sidePointOne.x,
+						sidePointTwo.x };
+				int[] arrowY = new int[] { toPoint.y, sidePointOne.y,
+						sidePointTwo.y };
+				int arrowN = 3;
+				Polygon arrowPolygon = new Polygon(arrowX, arrowY, arrowN);
+				g2.setColor(Color.black);
+				g2.fill(arrowPolygon);
+
+				// Shape nachziehen: Seite 1 und Seite 2 Dreieck
+				shapePolygon.addPoint(sidePointOne.x, sidePointOne.y);
+				shapePolygon.addPoint(sidePointTwo.x, sidePointTwo.y);
+				shapePolygon.npoints += 2;
 			}
 
-			// Daten für Pfeil bekannt, Pfeil zeichnen
-			Polygon arrowPolygon = new Polygon();
-			// Spitze immer gleich
-			arrowPolygon.addPoint(toPoint.x, toPoint.y);
-			arrowPolygon.addPoint(sidePointOne.x, sidePointOne.y);
-			arrowPolygon.addPoint(sidePointTwo.x, sidePointTwo.y);
-			// Noch einmal die Spitze zum abschliesen des Polygons
-			arrowPolygon.addPoint(toPoint.x, toPoint.y);
-			g2.fill(arrowPolygon);
-		}
-		// Gewicht wenn gewichtet
-		if (weighted) {
-			// Punkt für Gewichtanzeige
-			Point labelPointOnStraightLine = VisualizationCalculator
-					.getPointOnStraightLine(toPoint, fromPoint,
-							VisualizationCalculator.getLineWidth(fromPoint,
-									toPoint) / 2);
+			// Gewicht wenn gewichtet
+			if (weighted) {
+				// Gewicht Label an Punkt anzeigen
+				JLabel label = new JLabel("42");
 
-			// Gewicht Label an Punkt anzeigen
-			JLabel l = new JLabel("42");
-			l.setLocation(labelPointOnStraightLine);
+				// Punkt für Gewichtanzeige
+				Point labelPointOnStraightLine = VisualizationCalculator
+						.getPointOnStraightLine(toPoint, fromPoint,
+								VisualizationCalculator.getLineWidth(fromPoint,
+										toPoint) / 2);
+
+				// Verschieben zu Punkt
+				label.setLocation(labelPointOnStraightLine);
+
+				// Shape nachziehen: Bounds von Label-Rechteck
+				Rectangle labelBounds = label.getBounds();
+				shapePolygon.addPoint(labelBounds.x, labelBounds.y);
+				shapePolygon.addPoint(labelBounds.x, labelBounds.y
+						+ labelBounds.height);
+				shapePolygon.addPoint(labelBounds.x + labelBounds.width,
+						labelBounds.y);
+				shapePolygon.addPoint(labelBounds.x + labelBounds.width,
+						labelBounds.y + labelBounds.height);
+				shapePolygon.npoints += 4;
+			}
+
+			// Shape aus berechnetem Polygon
+			this.shape = shapePolygon;
+		} else {
+			// Shape für Linie immer gleich
+			this.shape = line.getBounds();
 		}
-		if (!directed && !weighted) {
-			// Shape für Linie
-			
-		}
-		// Shape setzen
-		this.shape = shapePolygon;
-		
+
 		this.setBorder(BorderFactory.createLineBorder(Color.red));
 	}
 

8bcbb5441bf810a8ecde4e66dda72e6d1b9b6d9a
bergs13 <bergs13@bfh.ch>
2013-11-15 15:46:42 +0100

    for chrigu

--- src/logic/GraphDataProcessor.java
+++ src/logic/GraphDataProcessor.java
@@ -1,9 +1,18 @@
 package logic;
 
-public class GraphDataProcessor {
+import logic.extlib.Graph;
+
+public class GraphDataProcessor<V,E> {
 
 	public GraphDataProcessor() {
 		// TODO Auto-generated constructor stub
 	}
-
+	public void exportGraph(Graph<V,E> g )
+	{
+		
+	}
+	public void importGraph(String filePath)
+	{
+		
+	}
 }

0e1156216e7d38f22d6915e0baea917b4e3a8e24
menzs2@bfh.ch <menzs2@bfh.ch>
2013-12-28 15:40:19 +0100

    AlgorithmDataProcessor and AlgorithmRunner ExceptionHandling

--- src/logic/AlgorithmDataProcessor.java
+++ src/logic/AlgorithmDataProcessor.java
@@ -8,67 +8,77 @@ import logic.extlib.IncidenceListGraph;
  * @author Stephan_2
  */
 public class AlgorithmDataProcessor {
+
     //
     private int graphListIndex;
     private final ArrayList<String> graphList;
     private GraphDataProcessor GDProcessor;
-    
-    public AlgorithmDataProcessor(){
+
+    public AlgorithmDataProcessor() {
         GDProcessor = new GraphDataProcessor();
         graphList = new ArrayList<>();
-        
+
     }
+
     //Start recording
-    public void resetGraphList(){
+    public void resetGraphList() {
         graphList.clear();
         graphListIndex = 0;
     }
-    
-    public void set(IncidenceListGraph graph){
-        String graphString = GDProcessor.constructStringFromGraph(graph);
-        graphList.add(graphString);
+
+    public void set(IncidenceListGraph graph) {
+        try{
+           String graphString = GDProcessor.constructStringFromGraph(graph);
+            graphList.add(graphString); 
+        }
+        catch(IllegalArgumentException err){
+            System.err.println("No Graph");
+        }
+        
     }
-    
+
     //returns the previous step
-    public IncidenceListGraph backward(){
-        if (graphListIndex == 0){
+    public IncidenceListGraph backward() {
+        if (graphListIndex == 0) {
             //Warning start of list
         }
         String graphString = graphList.get(--graphListIndex);
         IncidenceListGraph graph = GDProcessor.reconstructGraphFromString(graphString);
         return graph;
     }
-    
+
     //returns the next step
-    public IncidenceListGraph forward(){
-        if (graphListIndex == graphList.size()-1){
+    public IncidenceListGraph forward() {
+        if (graphListIndex == graphList.size() - 1) {
             //warning end of list
         }
         String graphString = graphList.get(++graphListIndex);
         IncidenceListGraph graph = GDProcessor.reconstructGraphFromString(graphString);
-        return graph; 
+        return graph;
     }
-    
+
     //returns the first step
-    public IncidenceListGraph first(){
+    public IncidenceListGraph first() {
         graphListIndex = 0;
-        String graphString = graphList.get(graphListIndex);
-        IncidenceListGraph graph = GDProcessor.reconstructGraphFromString(graphString);
-        return graph; 
-    }
-    
-    //returns the last step
-    public IncidenceListGraph last(){
-        
-        graphListIndex = graphList.size()-1;
         String graphString = graphList.get(graphListIndex);
         IncidenceListGraph graph = GDProcessor.reconstructGraphFromString(graphString);
         return graph;
     }
-    public ArrayList getGraphList(){
-       return graphList;
+
+    //returns the last step
+    public IncidenceListGraph last() {
+
+        graphListIndex = graphList.size() - 1;
+        String graphString = graphList.get(graphListIndex);
+        IncidenceListGraph graph = GDProcessor.reconstructGraphFromString(graphString);
+        return graph;
     }
-    public void setGraphList(ArrayList GraphlistToImport){
+
+    public ArrayList getGraphList() {
+        return graphList;
+    }
+
+    public void setGraphList(ArrayList GraphlistToImport) {
         this.resetGraphList();
         graphList.addAll(GraphlistToImport);
     }
--- src/logic/AlgorithmRunner.java
+++ src/logic/AlgorithmRunner.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package logic;
 
 import java.util.logging.Level;
@@ -13,37 +12,29 @@ import java.util.logging.Logger;
  *
  * @author Stephan_2
  */
-public class AlgorithmRunner implements Runnable{
-    
+public class AlgorithmRunner implements Runnable {
+
     private AlgorithmDataProcessor algorithmDataProcessor;
-    private boolean stepByStep; 
-    
-    public AlgorithmRunner(AlgorithmDataProcessor processor){
+    private boolean stepByStep;
+
+    public AlgorithmRunner(AlgorithmDataProcessor processor) {
         if (null == processor) {
-			throw new IllegalArgumentException("no AlgoritDataProcessor set for AlgorithmRunner");
-		}
+            throw new IllegalArgumentException("no AlgoritDataProcessor set for AlgorithmRunner");
+        }
         algorithmDataProcessor = processor;
     }
-    
+
     @Override
     public void run() {
         this.algorithmDataProcessor.first();
-        if (true == stepByStep){
-            
-            for(int k = 0; k<=algorithmDataProcessor.getGraphList().size(); k++ ){
-                try {
+        for (int k = 0; k <= algorithmDataProcessor.getGraphList().size(); k++) {
+            try {
                 Thread.sleep(3000);
                 algorithmDataProcessor.forward();
             } catch (InterruptedException ex) {
                 Logger.getLogger(AlgorithmRunner.class.getName()).log(Level.SEVERE, null, ex);
             }
-            }
-        }
-        
-        else {
-            
         }
     }
-    
+
 }
-    
